06ef8451ba157c60bcb8e48c9928eb4e2dca9f2c
Rudransh Pratap Singh <87255169+CoderFleet@users.noreply.github.com>
2024-07-19 13:15:35 +0530

    Guest Management & Room Search

--- main.c
+++ main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_ROOMS 100
+#define MAX_NAME_LENGTH 50
 
 typedef struct {
     int roomNumber;
     int isOccupied;
+    char guestName[MAX_NAME_LENGTH];
 } Room;
 
 Room rooms[MAX_ROOMS];
 
-void pause() {
-    printf("Press Enter to continue...");
-    while (getchar() != '\n');
-    getchar(); // Consume the newline character
-}
+void initializeRooms();
+void displayMenu();
+void checkIn();
+void checkOut();
+void viewRooms();
+void searchRoom();
+void waitForUserInput();
 
 void initializeRooms() {
     for (int i = 0; i < MAX_ROOMS; i++) {
         rooms[i].roomNumber = i + 1;
         rooms[i].isOccupied = 0;
+        strcpy(rooms[i].guestName, "");
     }
 }
 
@@ -28,36 +34,46 @@ void displayMenu() {
     printf("1. Check In\n");
     printf("2. Check Out\n");
     printf("3. View Rooms\n");
-    printf("4. Exit\n");
+    printf("4. Search Room\n");
+    printf("5. Exit\n");
     printf("Enter your choice: ");
 }
 
 void checkIn() {
     int roomNumber;
+    char guestName[MAX_NAME_LENGTH];
     printf("Enter room number to check in: ");
     scanf("%d", &roomNumber);
+    getchar();
 
     if (roomNumber > 0 && roomNumber <= MAX_ROOMS) {
         if (rooms[roomNumber - 1].isOccupied) {
             printf("Room %d is already occupied.\n", roomNumber);
         } else {
+            printf("Enter guest name: ");
+            fgets(guestName, MAX_NAME_LENGTH, stdin);
+            guestName[strcspn(guestName, "\n")] = '\0';
+
             rooms[roomNumber - 1].isOccupied = 1;
-            printf("Checked in to room %d.\n", roomNumber);
+            strcpy(rooms[roomNumber - 1].guestName, guestName);
+            printf("Checked in to room %d for %s.\n", roomNumber, guestName);
         }
     } else {
         printf("Invalid room number.\n");
     }
-    pause();
+    waitForUserInput();
 }
 
 void checkOut() {
     int roomNumber;
     printf("Enter room number to check out: ");
     scanf("%d", &roomNumber);
+    getchar();
 
     if (roomNumber > 0 && roomNumber <= MAX_ROOMS) {
         if (rooms[roomNumber - 1].isOccupied) {
             rooms[roomNumber - 1].isOccupied = 0;
+            strcpy(rooms[roomNumber - 1].guestName, "");
             printf("Checked out from room %d.\n", roomNumber);
         } else {
             printf("Room %d is not occupied.\n", roomNumber);
@@ -65,15 +81,42 @@ void checkOut() {
     } else {
         printf("Invalid room number.\n");
     }
-    pause();
+    waitForUserInput();
 }
 
 void viewRooms() {
     printf("Room Status:\n");
     for (int i = 0; i < MAX_ROOMS; i++) {
-        printf("Room %d: %s\n", rooms[i].roomNumber, rooms[i].isOccupied ? "Occupied" : "Available");
+        printf("Room %d: %s", rooms[i].roomNumber, rooms[i].isOccupied ? "Occupied" : "Available");
+        if (rooms[i].isOccupied) {
+            printf(" - Guest: %s", rooms[i].guestName);
+        }
+        printf("\n");
     }
-    pause();
+    waitForUserInput();
+}
+
+void searchRoom() {
+    int roomNumber;
+    printf("Enter room number to search: ");
+    scanf("%d", &roomNumber);
+    getchar();
+
+    if (roomNumber > 0 && roomNumber <= MAX_ROOMS) {
+        if (rooms[roomNumber - 1].isOccupied) {
+            printf("Room %d is occupied by %s.\n", roomNumber, rooms[roomNumber - 1].guestName);
+        } else {
+            printf("Room %d is available.\n", roomNumber);
+        }
+    } else {
+        printf("Invalid room number.\n");
+    }
+    waitForUserInput();
+}
+
+void waitForUserInput() {
+    printf("Press Enter to continue...");
+    while (getchar() != '\n');
 }
 
 int main() {
@@ -84,7 +127,7 @@ int main() {
     while (1) {
         displayMenu();
         scanf("%d", &choice);
-        getchar(); // Consume newline character left by scanf
+        getchar();
 
         switch (choice) {
             case 1:
@@ -97,11 +140,14 @@ int main() {
                 viewRooms();
                 break;
             case 4:
+                searchRoom();
+                break;
+            case 5:
                 printf("Exiting...\n");
                 exit(0);
             default:
                 printf("Invalid choice, please try again.\n");
-                pause();
+                waitForUserInput();
         }
     }
 
